dcebb5d3d0f06fbee9fc29ac2b3e41e3f3225765
Mariano Soto <marianojwl@gmail.com>
2023-09-28 18:42:24 -0300

    final and docs

--- QuickThumb.php
+++ QuickThumb.php
@@ -1,15 +1,29 @@
 <?php
-namespace marianojwl {
+namespace marianojwl\QuickThumb {
     class QuickThumb {
         public static function make(string $path_origin) {
+            // Get information about the original file
             $fileInfo = pathinfo($path_origin);
-            $sufix = '_thumb';
-            $path_result = $fileInfo['dirname'] .  $fileInfo['filename'] . $sufix . '.' . $fileInfo['extension'];
+            
+            // Define the suffix for the thumbnail
+            $suffix = '_thumb';
+            
+            // Generate the path for the thumbnail
+            $path_result = $fileInfo['dirname'] . '/' . $fileInfo['filename'] . $suffix . '.' . $fileInfo['extension'];
+
+            // Create an image resource from the original image
             $gd = self::imageCreate($path_origin, $fileInfo['extension']);
-            if (self::createThumb($gd, $path_result, $fileInfo['extension']) )
+
+            // If thumbnail creation is successful, return the path; otherwise, return null
+            if (self::createThumb($gd, $path_result, $fileInfo['extension'])) {
                 return $path_result;
+            } else {
+                return null;
+            }
         }
-        private static function imageCreate($path, $ext){
+
+        private static function imageCreate($path, $ext) {
+            // Create an image resource based on the file extension
             switch($ext) {
                 case "jpg":
                 case "jpeg":
@@ -18,11 +32,14 @@ namespace marianojwl {
                     return imagecreatefrompng($path);
                 case "gif":
                     return imagecreatefromgif($path);
-                case "bmp":
-                    return imagecreatefrombmp($path);
+                default:
+                    // Handle unsupported image types gracefully
+                    return null;
             }
         }
-        private static function imageSave($gd, $path, $ext){
+
+        private static function imageSave($gd, $path, $ext) {
+            // Save the image resource to a file based on the file extension
             switch($ext) {
                 case "jpg":
                 case "jpeg":
@@ -31,36 +48,42 @@ namespace marianojwl {
                     return imagepng($gd, $path);
                 case "gif":
                     return imagegif($gd, $path);
-                case "bmp":
-                    return imagebmp($gd, $path);
+                default:
+                    return false;
             }
         }
 
         private static function createThumb($originalImage, $path_result, $ext, $thumbWidth = 100, $thumbHeight = null ) {
+            // Get the dimensions of the original image
             $originalWidth = imagesx($originalImage);
             $originalHeight = imagesy($originalImage);
+
+            // Calculate the aspect ratio
             $aspectRatio = $originalWidth / $originalHeight;
 
-            if($thumbHeight) {
-                    $newWidth = $thumbHeight * $aspectRatio;
-                    $newHeight = $thumbHeight;
+            if ($thumbHeight) {
+                $newWidth = $thumbHeight * $aspectRatio;
+                $newHeight = $thumbHeight;
             } else {
-                    $newWidth = $thumbWidth;
-                    $newHeight = $newWidth / $aspectRatio;
+                $newWidth = $thumbWidth;
+                $newHeight = $newWidth / $aspectRatio;
             }
-        
+
             // Create a new GD image resource for the resized image
             $resizedImage = imagecreatetruecolor($newWidth, $newHeight);
-        
+
             // Use a better interpolation method for resizing
             imagecopyresampled($resizedImage, $originalImage, 0, 0, 0, 0, $newWidth, $newHeight, $originalWidth, $originalHeight);
 
+            // Save the resized image
             $result = self::imageSave($resizedImage, $path_result, $ext);
-            
+
+            // Destroy the image resources to free up memory
             imagedestroy($originalImage);
             imagedestroy($resizedImage);
 
             return $result;
         }
     }
+
 }
--- README.md
+++ README.md
@@ -1,1 +1,79 @@
-# QuickThumb
+Certainly! Below is a README.md template for your GitHub repository that provides an overview of the `marianojwl\QuickThumb` class and instructions on how to use it.
+
+```markdown
+# QuickThumb - PHP Image Thumbnail Generator
+
+QuickThumb is a PHP class that simplifies the process of generating image thumbnails from existing image files. It offers a convenient way to create resized versions of images while maintaining their aspect ratios.
+
+## Features
+
+- Generates image thumbnails with specified dimensions.
+- Supports common image formats: JPEG, PNG, GIF.
+- Automatically calculates thumbnail dimensions based on aspect ratio.
+- Simple and easy-to-use API.
+
+## Installation
+
+You can include the QuickThumb class in your PHP project using composer or by manually including it in your project.
+
+### Composer Installation
+
+To install QuickThumb via Composer, run the following command in your project directory:
+
+```bash
+composer require marianojwl/quickthumb
+```
+
+### Manual Installation
+
+1. Download the [QuickThumb.php](https://github.com/yourusername/yourrepository/blob/main/QuickThumb.php) file from this repository.
+2. Place the `QuickThumb.php` file in your project directory.
+
+## Usage
+
+Here's how you can use QuickThumb to generate a thumbnail from an image:
+
+```php
+<?php
+require_once 'QuickThumb.php'; // Include the QuickThumb class
+
+use marianojwl\QuickThumb\QuickThumb;
+
+// Path to the original image
+$originalImagePath = '/path/to/your/original/image.jpg';
+
+// Generate the thumbnail using the QuickThumb class
+$thumbnailPath = QuickThumb::make($originalImagePath);
+
+if ($thumbnailPath) {
+    echo "Thumbnail created successfully. Path: $thumbnailPath";
+} else {
+    echo "Thumbnail creation failed.";
+}
+?>
+```
+
+Replace `'/path/to/your/original/image.jpg'` with the actual path to the image from which you want to generate a thumbnail.
+
+## Configuration
+
+You can configure the following aspects of thumbnail generation:
+
+- Thumbnail dimensions (width and height) - Specify the desired width and height for the thumbnail.
+- Supported image formats - QuickThumb supports JPEG, PNG, and GIF formats by default. You can extend this support by modifying the `imageCreate()` and `imageSave()` methods in the `QuickThumb` class.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
+
+## Author
+
+- [Mariano Soto](https://github.com/marianojwl)
+
+## Contributions
+
+Contributions to this project are welcome. Feel free to open issues and pull requests.
+
+```
+
+Please make sure to replace the placeholders like `[Your Name]`, `[Your GitHub Username]`, and `[Your Repository URL]` with your actual information. You should also update the installation instructions to reflect how users can install your QuickThumb class in their projects.
